4b785b2299647561986bb5d4202bb76013d94119
Osvaldo Trejo <OSVALDO.TREJO1@ibm.com>
2020-08-08 13:32:52 -0500

    new mode

--- gars_hackaton/src/Components/Form.js
+++ gars_hackaton/src/Components/Form.js
@@ -4,44 +4,8 @@ import '../Assets/Styles/Form.css'
 
 
 
-const Form = () => {
-    const [dataForm, setDataForm] = useState({})
-    const handleChange = (event) => {
-        
-        setDataForm({
-            ...dataForm,
-            id_random:  Math.floor(Math.random() * 1000),
-            [event.target.name]: event.target.value
-        })
+const Form = (props) => {
 
-        console.log(dataForm)
-    }
-
-    const submitEvent = async () => {
-        /*
-            Aqui deben de crear el codigo para poder enviar la informacion a la base de datos haciendo la validación
-            de que ningun campo este vacio
-        */
-       console.log(dataForm)
-
-       try {
-            let res = await fetch('newOrder/', {
-                method: 'POST',
-                body: JSON.stringify(dataForm),
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Access-Control-Allow-Origin': '*'
-                }
-            })
-            alert('Sin problemas')
-            console.log(res)
-       }
-       catch(err){
-           console.log(err)
-       }
-       
-       
-    }
     return(
         <>
             <div className="wrapper-form">
@@ -50,32 +14,32 @@ const Form = () => {
                     <span>Enterprise Name:</span>
                     <input 
                         type="text" 
-                        onChange={handleChange} 
+                        onChange={props.handleChange} 
                         name="enterprise_name" 
                         placeholder="Enterprise Name"/>
                     <span>Address:</span>
                     <input 
                         type="text" 
-                        onChange={handleChange} 
+                        onChange={props.handleChange} 
                         name="address" 
                         placeholder="Address"/>
                     <span>Email:</span>
                     <input 
                         type="email" 
-                        onChange={handleChange} 
+                        onChange={props.handleChange} 
                         name="email" 
                         placeholder="Email"/>
                     <span>Enterprise key(RFC):</span>
                     <input 
                         type="text" 
-                        onChange={handleChange} 
-                        name="enterprise_key" 
+                        onChange={props.handleChange}
+                        name="enterpriseKey" 
                         placeholder="Enterprise key(RFC)"/>
                     <span>Description:</span>
                     <textarea 
-                        onChange={handleChange} 
+                        onChange={props.handleChange} 
                         name="description"/>
-                    <div  className="button-form" onClick={submitEvent}>Submit</div>
+                    <div  className="button-form" onClick={props.submitEvent}>Submit</div>
                 </form>
             </div>
         </>
--- gars_hackaton/src/Containers/Home.js
+++ gars_hackaton/src/Containers/Home.js
@@ -10,13 +10,52 @@ import '../Assets/Styles/home.css'
 const Home = () => {
 
     const [dataCard, setDataCard] = useState([])
+    const [dataForm, setDataForm] = useState({})
 
-    useEffect(() => {
-        fetch('orders/')
+    const handleChange = (event) => {
+        
+        setDataForm({
+            ...dataForm,
+            id_random: Math.floor(Math.random() * 1000),
+            [event.target.name]: event.target.value
+        })
+        console.log(dataForm)
+    }
+
+    const getData = async () => {
+        fetch('/orders')
         .then(res => res.json())
         .then(data => {
             setDataCard(data.data)
         })
+    }
+
+    const submitEvent = async () => {
+        /*
+            Aqui deben de crear el codigo para poder enviar la informacion a la base de datos haciendo la validación
+            de que ningun campo este vacio
+        */ 
+
+       try {
+            // let res = await fetch('newOrder/', {
+            //     method: 'POST',
+            //     body: JSON.stringify(dataForm),
+            //     headers: {
+            //         'Content-Type': 'application/json',
+            //         'Access-Control-Allow-Origin': '*'
+            //     }
+            // })
+            // alert('Sin problemas')
+            getData()
+            setDataForm({})
+       }
+       catch(err){
+           console.log(err)
+       }
+    }
+
+    useEffect(() => {
+        getData()
     }, [])
 
 
@@ -25,7 +64,7 @@ const Home = () => {
             <Navbar />
             <Header />
             <div className="wrapper-main-section">
-                <Form />
+                <Form submitEvent={submitEvent} handleChange={handleChange}/>
                 <Cards dataCard={dataCard} />
             </div>
         </>
