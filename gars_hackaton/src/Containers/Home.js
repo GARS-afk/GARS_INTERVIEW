c60ddbd16fd58f61c39ac4f76459662d37b36805
Angel Zacarias <luis.zacariasm@alumnos.udg.mx>
2020-08-14 14:50:03 -0500

    Team 1

--- gars_hackaton/src/Containers/Home.js
+++ gars_hackaton/src/Containers/Home.js
@@ -1,102 +1,125 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 //Componentes
-import Navbar from '../Components/Navbar'
-import Form from '../Components/Form'
-import Header from '../Components/Header'
-import Cards from '../Components/Cards'
+import Navbar from "../Components/Navbar";
+import Form from "../Components/Form";
+import Header from "../Components/Header";
+import Cards from "../Components/Cards";
 //styles
-import '../Assets/Styles/home.css'
+import "../Assets/Styles/home.css";
 
 const Home = () => {
+  const [dataCard, setDataCard] = useState([]);
+  const [dataForm, setDataForm] = useState({});
 
-    const [dataCard, setDataCard] = useState([])
-    const [dataForm, setDataForm] = useState({})
-
-    const acceptCard = async (id_random) => {
-        let data = {
-            ID_ORDER: id_random,
-            STATUS: 'accept'
-        }
-        try {
-            await fetch('/statusOrder', {
-                method:'PUT',
-                body: JSON.stringify(data),
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Access-Control-Allow-Origin': '*'
-                }
-            })
-            getData()
-        }
-        catch(error) {
-            console.log(error)
-        }
+  const acceptCard = async (id_random) => {
+    let data = {
+      ID_ORDER: id_random,
+      STATUS: "accept",
+    };
+    try {
+      await fetch("/statusOrder", {
+        method: "PUT",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+      });
+      getData();
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    const handleChange = (event) => {
-        setDataForm({
-            ...dataForm,
-            //enterpriseKey: Descomentar e ingresar el codigo que genere el enterpriseKey con los primeros tres caracteres de la empresa, y seis numeros aleatorios
-            [event.target.name]: event.target.value
-        })
+  const handleChange = (event) => {
+    setDataForm({
+      ...dataForm,
+      //enterpriseKey: Descomentar e ingresar el codigo que genere el enterpriseKey con los primeros tres caracteres de la empresa, y seis numeros aleatorios
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const getData = async () => {
+    fetch("/orders")
+      .then((res) => res.json())
+      .then((data) => {
+        setDataCard(data.data);
+      });
+  };
+
+  const rejectOrder = async (orderId) => {
+    try {
+      let body = {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify({
+          ID_ORDER: orderId,
+          STATUS: "reject",
+        }),
+      };
+      fetch("/statusOrder", body)
+        .then((res) => res.json())
+        .then((data) => {
+          getData();
+        });
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    const getData = async () => {
-        fetch('/orders')
-        .then(res => res.json())
-        .then(data => {
-            setDataCard(data.data)
-        })
+  const submitEvent = async () => {
+    let randomKey;
+    randomKey = Math.floor(Math.random() * (999999 - 100000)) + 100000;
+    randomKey = "TES" + randomKey;
+    try {
+      let body = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify({
+          ENTERPRISE_NAME: "TEST CO",
+          ADDRESS: "testing address",
+          EMAIL: "testing@email.com",
+          ID_ORDER: "",
+          ENTERPRISE_KEY: randomKey,
+          STATUS: "new",
+        }),
+      };
+      console.log("dos");
+      fetch("/newOrder", body)
+        .then((res) => res.json())
+        .then((data) => {
+          getData();
+        });
+      console.log("ok");
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    const rejectOrder = async ( orderId ) =>{
-        try{
-            let body = {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Access-Control-Allow-Origin': '*'
-                },
-                body: JSON.stringify({
-                    ID_ORDER: orderId,
-                    STATUS: 'reject'
-                })
-            }
-            fetch('/statusOrder', body)
-            .then(res => res.json() )
-            .then( data => {
-                getData()
-            })
-        }
-        catch(error){
-            console.log(error)
-        }
-       
-    } 
+  useEffect(() => {
+    getData();
+  }, []);
 
-    const submitEvent = async () => {
-        /*
-            Aqui deben de crear el codigo para poder enviar la informacion a la base de datos haciendo la validación
-            de que ningun campo este vacio
-        */ 
-    }
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <div className="wrapper-main-section">
+        <Form submitEvent={submitEvent} handleChange={handleChange} />
+        <Cards
+          dataCard={dataCard}
+          acceptCard={acceptCard}
+          rejectOrder={rejectOrder}
+        />
+      </div>
+    </>
+  );
+};
 
-    useEffect(() => {
-        getData()
-    }, [])
-
-
-    return(
-        <>
-            <Navbar />
-            <Header />
-            <div className="wrapper-main-section">
-                <Form submitEvent={submitEvent} handleChange={handleChange}/>
-                <Cards dataCard={dataCard} acceptCard={acceptCard} rejectOrder={rejectOrder}/>
-            </div>
-        </>
-
-    )
-}
-
-export default Home
+export default Home;
--- server/index.js
+++ server/index.js
@@ -1,126 +1,128 @@
-const express = require('express')
-require('dotenv').config()
-const ibm_db = require('ibm_db')
-const app = express()
+const express = require("express");
+require("dotenv").config();
+const ibm_db = require("ibm_db");
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/orders', (req, res) => {
-    ibm_db.open(process.env.DB, ( err, con ) => {
-        if( err ) {
+app.get("/orders", (req, res) => {
+  ibm_db.open(process.env.DB, (err, con) => {
+    if (err) {
+      res.status(500).json({
+        mensaje: "Error al intentar concectar con la DB",
+        error: err,
+      });
+    } else {
+      con.query("SELECT * FROM JFV11323.ORDERS", (err, data) => {
+        if (err) {
+          res.status(500).json({
+            mensaje: "Error al ejecutar el query",
+            error: err,
+          });
+          con.close();
+        } else {
+          res.status(200).json({
+            mensaje: "Consulta realizada con exito",
+            data: data,
+          });
+          con.close();
+        }
+      });
+    }
+  });
+});
+
+app.post("/newOrder", (req, res) => {
+  //let id_random: Descomentar e ingresar el codigo que genere un nuemero aleatorio,
+  let id_random = Math.floor(Math.random() * 9999999);
+  ibm_db.open(process.env.DB, (err, con) => {
+    if (err) {
+      res.status(500).json({
+        mensaje: "Error al intentar concectar con la DB",
+        error: err,
+      });
+    } else {
+      con.query(
+        `INSERT INTO JFV11323.ORDERS(ID_ORDER,ENTERPRISE_NAME,ADDRESS,EMAIL,ENTERPRISE_KEY,DESCRIPTION,STATUS) VALUES('${id_random}',' ${req.body.ENTERPRISE_NAME}',' ${req.body.ADDRESS}','${req.body.EMAIL}','${req.body.ENTERPRISE_KEY}','${req.body.DESCRIPTION}','${req.body.STATUS}')`,
+        (err, data) => {
+          if (err) {
             res.status(500).json({
-                mensaje: 'Error al intentar concectar con la DB',
-                error: err
-            })
+              mensaje: "Error al ejecutar el query",
+              error: err,
+            });
+            con.close();
+          } else {
+            res.status(200).json({
+              mensaje: "nueva orden registrada",
+            });
+            con.close();
+          }
         }
-        else {
-            con.query('SELECT * FROM JFV11323.ORDERS', (err, data) => {
-                if( err ) {
-                    res.status(500).json({
-                        mensaje:'Error al ejecutar el query',
-                        error: err
-                    })
-                    con.close();
-                }
-                else {
-                    res.status(200).json({
-                        mensaje: 'Consulta realizada con exito',
-                        data: data
-                    }) 
-                    con.close()
-                }
-            })
-        }
-    })
-})
+      );
+    }
+  });
+});
 
-app.post('/newOrder', ( req, res ) => {
-     //let id_random: Descomentar e ingresar el codigo que genere un nuemero aleatorio,
-    ibm_db.open(process.env.DB, ( err, con ) => {
-        if( err ) {
+app.put("/statusOrder", (req, res) => {
+  ibm_db.open(process.env.DB, (err, con) => {
+    if (err) {
+      res.status(500).json({
+        mensaje: "Error Conectarse a la db",
+        error: err,
+      });
+    } else {
+      con.query(
+        `UPDATE JFV11323.ORDERS SET STATUS = '${req.body.STATUS}' WHERE ID_ORDER = ${req.body.ID_ORDER}`,
+        (err, data) => {
+          if (err) {
             res.status(500).json({
-                mensaje: 'Error al intentar concectar con la DB',
-                error: err
-            })
+              mensaje: "Error ejecutar el query",
+              error: err,
+            });
+            con.close();
+          } else {
+            res.status(200).json({
+              mensaje: "Status actualizado",
+              data: data,
+            });
+            con.close();
+          }
         }
-        else {
-            con.query(/* aqui deben colocar el query para insertar los datos que vienen del front*/'', ( err, data ) => {
-                if( err ) {
-                    res.status(500).json({
-                        mensaje: 'Error al ejecutar el query' ,
-                        error: err
-                    })
-                    con.close();
-                }
-                else {
-                    res.status(200).json({
-                        mensaje: 'nueva orden registrada'
-                    })
-                    con.close();
-                }
-            })
-        }
-    })
-})
+      );
+    }
+  });
+});
 
-app.put('/statusOrder', ( req, res ) =>{
-    ibm_db.open(process.env.DB, (err, con) => {
-        if( err ) {
+app.delete("/deleteOrder", (req, res) => {
+  ibm_db.open(process.env.DB, (err, con) => {
+    if (err) {
+      res.status(500).json({
+        mensaje: "Error Conectarse a la db",
+        error: err,
+      });
+    } else {
+      con.query(
+        `DELETE * FROM JFV11323 WHERE ID_ORDER = ${req.body.ID_ORDER}`,
+        (err, data) => {
+          if (err) {
             res.status(500).json({
-                mensaje:'Error Conectarse a la db',
-                error: err
-            })
+              mensaje: "Error ejecutar el query",
+              error: err,
+            });
+            con.close();
+          } else {
+            res.status(200).json({
+              mensaje: "Opcion eliminada",
+              data: data,
+            });
+            con.close();
+          }
         }
-        else {
-            con.query(`UPDATE JFV11323.ORDERS SET STATUS = '${ req.body.STATUS }' WHERE ID_ORDER = ${req.body.ID_ORDER}`, (err, data) => {
-                if( err ) {
-                    res.status(500).json({
-                        mensaje:'Error ejecutar el query',
-                        error: err
-                    })
-                    con.close();
-                }
-                else {
-                    res.status(200).json({
-                        mensaje: 'Status actualizado',
-                        data: data
-                    })
-                    con.close();
-                }
-            })
-        }
-    })
-})
+      );
+    }
+  });
+});
 
-app.delete('/deleteOrder', (req, res) => {
-    ibm_db.open(process.env.DB, (err, con) => {
-        if( err ) {
-            res.status(500).json({
-                mensaje:'Error Conectarse a la db',
-                error: err
-            })
-        }
-        else {
-            con.query(`DELETE * FROM JFV11323 WHERE ID_ORDER = ${ req.body.ID_ORDER }`, (err, data) => {
-                if( err ) {
-                    res.status(500).json({
-                        mensaje:'Error ejecutar el query',
-                        error: err
-                    })
-                    con.close();
-                }
-                else {
-                    res.status(200).json({
-                        mensaje: 'Opcion eliminada',
-                        data: data
-                    })
-                    con.close();
-                }
-            })
-        }
-    })
-})
-
-app.listen( process.env.PORT, () => {
-    console.log(`Example app listening at http://localhost:${ process.env.PORT }`)
-  })
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening at http://localhost:${process.env.PORT}`);
+});
